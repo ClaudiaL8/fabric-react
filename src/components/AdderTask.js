98b3f8c25b3154a78c00168e7e56158a007bc2f8
Claudia Lorien Calero <claudia.calero@ultimasoftware.com>
2019-09-27 16:02:29 +0200

    add task item component

--- src/components/AdderTask.js
+++ src/components/AdderTask.js
@@ -48,6 +48,7 @@ export default function AdderTask() {
                 // iconProps={{ iconName: 'Add' }}
                 onClick={handleClick}
                 allowDisabledFocus
+                ariaLabel='Add task'
                 //disabled
                // checked
                 />
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import {  Stack, Checkbox,IconButton } from 'office-ui-fabric-react';
+import { log } from 'util';
+
 
 export default function TaskItem() {
+    const handleEdit = () => {
+        console.log('edit');
+        
+    }
+
+    const handleDelete = () => {
+        console.log('delete');
+        
+
+    }
+
     return (
-      <div className="">
-          <p>TaskItem</p>
-      </div>
+      <Stack horizontal horizontalAlign='space-between' verticalAlign='center'>
+        <Checkbox 
+            label = 'tarea'
+            onChange={(e)=>{console.log(e.target.checked)}}
+            checked = {true}
+        />
+        <Stack horizontal>
+            <IconButton 
+                iconProps={{ iconName: 'Edit' }}
+                ariaLabel='Edit'
+                onClick={handleEdit}
+            />
+            <IconButton 
+                iconProps={{ iconName: 'Cancel' }}
+                ariaLabel='Delete'
+                onClick={handleDelete}
+            />
+        </Stack>
+      </Stack>
     );
   }
