556293f5cd08c4db2e82432d9a66a1313415b449
Claudia Lorien Calero <claudia.calero@ultimasoftware.com>
2019-10-01 15:44:32 +0200

    cleared code

--- src/ToDoApp.js
+++ src/ToDoApp.js
@@ -2,7 +2,35 @@ import React from 'react';
 import './App.css';
 import AdderTask from './components/AdderTask';
 import TaskList from './components/TaskList';
-import Summary from './components/Summary';
+
+import { loadTheme } from 'office-ui-fabric-react';
+
+loadTheme({
+  palette: {
+    themePrimary: '#d4007d',
+    themeLighterAlt: '#fdf3f9',
+    themeLighter: '#f8d0e8',
+    themeLight: '#f2a9d4',
+    themeTertiary: '#e55cad',
+    themeSecondary: '#d91a8b',
+    themeDarkAlt: '#be0070',
+    themeDark: '#a1005f',
+    themeDarker: '#770046',
+    neutralLighterAlt: '#f8f8f8',
+    neutralLighter: '#f4f4f4',
+    neutralLight: '#eaeaea',
+    neutralQuaternaryAlt: '#dadada',
+    neutralQuaternary: '#d0d0d0',
+    neutralTertiaryAlt: '#c8c8c8',
+    neutralTertiary: '#bab8b7',
+    neutralSecondary: '#a3a2a0',
+    neutralPrimaryAlt: '#8d8b8a',
+    neutralPrimary: '#323130',
+    neutralDark: '#605e5d',
+    black: '#494847',
+    white: '#ffffff',
+  }
+});
 
 export default function ToDoApp() {
     return (
@@ -10,7 +38,6 @@ export default function ToDoApp() {
         <h1>todos</h1>
         <AdderTask/>
         <TaskList/>
-        <Summary/>
         </div>
     );
 }
--- src/components/FilterList.js
+++ src/components/FilterList.js
@@ -1,57 +1,72 @@
 import React, { useContext } from 'react';
-import { Pivot, PivotItem } from 'office-ui-fabric-react';
+import { Pivot, PivotItem, Stack, DefaultButton } from 'office-ui-fabric-react';
 import { Context } from '../context/Context';
 import TaskItem from './TaskItem';
 
 export default function FilterList() {
-    const { tasks } = useContext(Context);
+    const { tasks, setTasks } = useContext(Context);
     let complete = tasks.filter(task => task.checked === true);
     let actives = tasks.filter(task => task.checked === false);
+    let activesLength = actives.length;
+
+    const handleClick = (e) => {
+        const completeFilter = tasks.filter(task => task.checked === false)
+        setTasks(completeFilter)
+    }
 
     return (
-        <Pivot>
-            <PivotItem headerText="all">
-                {tasks.map(item => {
-                    const { id, name, checked } = item;
-                    return (
-                        <li key={id}>
-                            <TaskItem
-                                id={id}
-                                name={name}
-                                checked={checked}
-                            />
-                        </li>
-                    )
-                })}
-            </PivotItem>
-            <PivotItem headerText="active" >
-                {actives.map(item => {
-                    const { id, name, checked } = item;
-                    return (
-                        <li key={id}>
-                            <TaskItem
-                                id={id}
-                                name={name}
-                                checked={checked}
-                            />
-                        </li>
-                    )
-                })}
-            </PivotItem>
-            <PivotItem headerText="completed">
-                {complete.map(item => {
-                    const { id, name, checked } = item;
-                    return (
-                        <li key={id}>
-                            <TaskItem
-                                id={id}
-                                name={name}
-                                checked={checked}
-                            />
-                        </li>
-                    )
-                })}
-            </PivotItem>
-        </Pivot>
+        <>
+            <Pivot>
+                <PivotItem headerText="all">
+                    {tasks.map(item => {
+                        const { id, name, checked } = item;
+                        return (
+                            <li key={id}>
+                                <TaskItem
+                                    id={id}
+                                    name={name}
+                                    checked={checked}
+                                />
+                            </li>
+                        )
+                    })}
+                </PivotItem>
+                <PivotItem headerText="active" >
+                    {actives.map(item => {
+                        const { id, name, checked } = item;
+                        return (
+                            <li key={id}>
+                                <TaskItem
+                                    id={id}
+                                    name={name}
+                                    checked={checked}
+                                />
+                            </li>
+                        )
+                    })}
+                </PivotItem>
+                <PivotItem headerText="completed">
+                    {complete.map(item => {
+                        const { id, name, checked } = item;
+                        return (
+                            <li key={id}>
+                                <TaskItem
+                                    id={id}
+                                    name={name}
+                                    checked={checked}
+                                />
+                            </li>
+                        )
+                    })}
+                </PivotItem>
+            </Pivot>
+            <Stack horizontal verticalAlign='center' >
+                <p>{activesLength} items left</p>
+                <DefaultButton
+                    text="Clear Completed"
+                    onClick={handleClick}
+                />
+            </Stack>
+        </>
     );
 }
